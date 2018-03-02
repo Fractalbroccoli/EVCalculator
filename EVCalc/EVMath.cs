b8573c58df94d94669931297448bacb4121a8a1d
Ricardo Mohammed <ricardo.m.aiesec@gmail.com>
2018-03-02 00:23:07 -0500

    Initial commit. Design contains 5 borders for displaying card images.

---
+++ EVCalc/EVMath.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVCalc
+{
+    public class EVMath
+    {
+
+
+
+
+        static public double SimplifyPotOddsRatio(double total, double bet)
+        {
+            const int percentMultiplier = 100;
+
+            double oddsAsPercent = (bet / total) * percentMultiplier;
+
+
+            return oddsAsPercent;
+        }
+
+
+        static public double ExpectedValue(double chanceOfA, double chanceOfB, double profitA, double profitB)
+        {
+            double expectedValue = 0;
+
+            expectedValue = (chanceOfA * profitA) + (chanceOfB * profitB);
+
+
+            return expectedValue;
+        }
+
+
+
+    }
+}
